5e7ba59b8f98860332e6a3d68043aaa86c50fd9e
jesicaacosta <ajesicadaiana@gmail.com>
2024-11-25 09:14:49 -0300

    Agregue pagina graficos

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, request, jsonify  # flask para crear la app y manejar solicitudes
-import pandas as pd  # 
-import plotly.express as px  #  generar graficos interactivos
-import plotly.io as pio  #  convertir graficos de Plotly a HTML
-
+from flask import Flask, render_template, request, jsonify  # Flask para crear la app y manejar solicitudes
+import pandas as pd  # Manejo de datos
+import plotly.express as px  # Generar gráficos interactivos
+import plotly.io as pio  # Convertir gráficos de Plotly a HTML
 
 app = Flask(__name__)
 
@@ -11,40 +10,41 @@ data = None
 
 @app.route('/')
 def home():
-    return render_template('index.html') #muestra la pagina principal
+    return render_template('index.html')  # Muestra la página principal
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    global data
-    file = request.files['file'] #archivo subido 
+    global data  #para hacer quye data sea global
+    file = request.files['file']  # Archivo subido 
     if file.filename == '':
         return "Por favor, selecciona un archivo CSV o Excel", 400
 
+    #lee el archivo
     data = pd.read_csv(file)
-    data_html = data.head(3).to_html()
-    numeric_columns = data.select_dtypes(include=['number']).columns.tolist()
+    data_html = data.head(5).to_html()
+    numeric_columns = data.select_dtypes(include=['number']).columns.tolist() #solo selecciona columnas numericas
     all_columns = data.columns.tolist()
 
-    #calcula las estdisicas del df cargado
+    # Calcula las estadísticas del DataFrame cargado
     stats = calculate_statistics(data)
 
     return render_template(
         'index.html', 
         data=data_html, 
         stats=stats, 
-        numeric_columns=numeric_columns,
-        all_columns=all_columns  # Enviar todas las columnas para eje X
+        numeric_columns=numeric_columns, # columnasn numericas
+        all_columns=all_columns  
     )
 
 def calculate_statistics(data):
     stats = {}
     for column in data.select_dtypes(include=['number']).columns:
         stats[column] = {
-            'count': data[column].count(), #cant valores no nulos 
-            'mean': round(data[column].mean(), 2), #pronedio redondeado a dos decimales
-            'std_dev': round(data[column].std(), 2), 
-            'min': data[column].min(),
-            'max': data[column].max()
+            'count': data[column].count(),  # Cantidad de valores no nulos 
+            'mean': round(data[column].mean(), 2),  # Promedio redondeado a dos decimales
+            'std_dev': round(data[column].std(), 2),  # Desviación estándar
+            'min': data[column].min(),  # Valor mínimo
+            'max': data[column].max()   # Valor máximo
         }
     return stats
 
@@ -60,30 +60,27 @@ def generate_graph():
     if x_column not in data.columns or y_column not in data.columns:
         return "Columnas seleccionadas no válidas", 400
 
-
-    #Grafico de dispersion 
-    fig1 = px.scatter(data, x=x_column, y=y_column, title=f'Gráfico de Dispersion {x_column} vs {y_column}')
+    # Gráfico de dispersión
+    fig1 = px.scatter(data, x=x_column, y=y_column, title=f'Gráfico de Dispersión {x_column} vs {y_column}')
     graph_html1 = pio.to_html(fig1, full_html=False)
     
-    #Grafico de barras 
-    fig2 = px.bar(data, x=x_column, y=y_column, title=f'Gráfico de barras  {x_column} vs {y_column} ', color = y_column)
+    # Gráfico de barras
+    fig2 = px.bar(data, x=x_column, y=y_column, title=f'Gráfico de Barras {x_column} vs {y_column}', color=y_column)
     graph_html2 = pio.to_html(fig2, full_html=False)
-    #full_html=False: GRAFICO sin la estructura completa de un documento HTML.
 
-    #Grafico de sectores 
-    fig3 = px.pie(data, names=x_column, values=y_column, title=f'Gráfico de sectores de {x_column} vs {y_column}')
-    graph_html3 = pio.to_html(fig3, full_html=False)
+    # Gráfico de sectores 10 
+    top_10 = data.nlargest(10, y_column)
+    fig_top = px.pie(top_10, names=x_column, values=y_column, title=f'Valores mas altos de {y_column} en relacion a {x_column}')
+    graph_html_top = pio.to_html(fig_top, full_html=False)
 
-
+    # Renderizar en `graficos.html`
     return render_template(
-        'index.html',
-        data=data.head(3).to_html(),
-        numeric_columns=data.select_dtypes(include=['number']).columns.tolist(),
-        all_columns=data.columns.tolist(),
+        'graficos.html',
         graph1=graph_html1,
         graph2=graph_html2,
-        graph3= graph_html3
-        
+        fig_top=graph_html_top,
+        x_column=x_column,
+        y_column=y_column
     )
 
 if __name__ == '__main__':
